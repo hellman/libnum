69260667d0ea72c474901dde143a738122abab99
Alexey Hellman <hellman1908@gmail.com>
2012-10-17 15:07:54 +0400

    nCk update

--- stuff.py
+++ stuff.py
@@ -18,8 +18,15 @@ def rev_grey_code(g):
 
 def nCk(n, k):
     """
-    Combinations number.
+    Combinations number
     """
-    return int(round(
-        reduce(operator.mul, (float(n-i)/(i+1) for i in range(k)), 1)
-    ))
+    if n < 0: raise ValueError("Invalid value for n: %s" % n)
+    if k < 0 or k > n: return 0
+    if k in (0, n): return 1
+    if k in (1, n-1): return n
+
+    low_min = 1
+    low_max = min(n, k)
+    high_min = max(1, n - k + 1)
+    high_max = n
+    return reduce(operator.mul, range(high_min, high_max + 1), 1) / reduce(operator.mul, range(low_min, low_max + 1), 1)
