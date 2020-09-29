b6fa7bffeae256e0238bb4875fa1c2c203eed919
hellman <hellman@users.noreply.github.com>
2020-09-29 12:50:21 +0200

    useless stub

--- libnum/__init__.py
+++ libnum/__init__.py
@@ -15,17 +15,4 @@ from .stuff import *
 from .chains import *
 from . import ecc
 
-
-# be careful when changing this name, don't use test*!
-def runtests():
-    """
-    Run all libnum tests and print output.
-    """
-    import os.path
-    from inspect import getsourcefile
-    from .tests import runtests as tests
-    testdir = os.path.dirname(os.path.abspath(getsourcefile(tests)))
-    importdir = os.path.abspath(testdir + "/../..")
-    tests.testit(importdir, testdir)
-
 # TODO: Add doctest after we have better docs
